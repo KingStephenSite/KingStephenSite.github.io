6eddba02af3d3065b12522b9d6418c8e84dea362
KingStephen <120324548+KingStephenSite@users.noreply.github.com>
2023-01-19 11:41:42 -0500

    added rap generator

---
+++ rap-song-generator/app.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+async function generateRapLyric() {
+  const prompt = "Write a rap about technology";
+
+  try {
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
+      prompt: prompt,
+      max_tokens: 100,
+      api_key: "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"
+    });
+
+    const rapLyric = response.data.choices[0].text;
+    console.log(rapLyric);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const responseContainer = document.getElementById("response-container");
+const response = document.getElementById("response");
+
+responseContainer.style.display = "block";
+response.innerText = "API response: " + JSON.stringify(apiResponse);
+
+
+generateRapLyric();
