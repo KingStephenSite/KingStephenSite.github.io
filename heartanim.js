4a89806800f1073c7cd9898f012fce9f225d665e
KingStephen <120324548+KingStephenSite@users.noreply.github.com>
2023-02-14 08:39:13 -0500

    minor update

---
+++ heartanim.js
@@ -0,0 +1,19 @@
+function createheart() {
+    const heart = document.createElement
+    ("div");
+    heart.classList.add("heart");
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+    heart.innerText ='❤️';  
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+setInterval(createheart,300);
